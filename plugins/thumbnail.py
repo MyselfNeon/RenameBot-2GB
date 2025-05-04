7fcd7d6cd7dcc58366333e265597f48587eeacc6
NeonAnurag <justasneon@gmail.com>
2025-05-04 18:31:22 +0530

    Update thumbnail.py

--- plugins/thumbnail.py
+++ plugins/thumbnail.py
@@ -8,18 +8,18 @@ async def viewthumb(client, message):
     if thumb:
        await client.send_photo(chat_id=message.chat.id, photo=thumb)
     else:
-        await message.reply_text("**You Don't Have Any Thumbnail ❌**") 
+        await message.reply_text("<b><i>You Don't Have Any Thumbnail ❌</i></b>") 
 		
 @Client.on_message(filters.private & filters.command(['del_thumb', 'delthumb']))
 async def removethumb(client, message):
     await jishubotz.set_thumbnail(message.from_user.id, file_id=None)
-    await message.reply_text("**Thumbnail Deleted Successfully 🗑️**")
+    await message.reply_text("<b><i>Thumbnail Deleted Successfully 🗑️</i></b>")
 	
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client, message):
-    mkn = await message.reply_text("Please Wait ...")
+    mkn = await message.reply_text("<b><i>Please Wait ...</i></b>")
     await jishubotz.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
-    await mkn.edit("**Thumbnail Saved Successfully ✅️**")
+    await mkn.edit("<i><b>Thumbnail Saved Successfully ✅️</b></i>")
 
 
 
