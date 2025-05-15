cf09d0d42e6dfa7a82834aa92781688f8cad0d70
NeonAnurag <justasneon@gmail.com>
2025-05-15 15:43:32 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -75,7 +75,7 @@ async def cb_handler(client, query: CallbackQuery):
 async def donate(client, message):
 	text = Txt.DONATE_TXT
 	keybord = InlineKeyboardMarkup([
-        			[InlineKeyboardButton("🦋 Aᴅᴍɪɴ",url = "https://t.me/OnionXbot"), 
+        			[InlineKeyboardButton("🦋 Aᴅᴍɪɴ",url = "https://t.me/MyselfNeon"), 
         			InlineKeyboardButton("✖️ Cʟᴏꜱᴇ",callback_data = "close") ]])
 	await message.reply_text(text = text,reply_markup = keybord)
 
