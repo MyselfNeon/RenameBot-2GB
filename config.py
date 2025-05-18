22929485afb0f2277a6f8050dfec6bc142f2ef3c
NeonAnurag <justasneon@gmail.com>
2025-05-18 16:41:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config(object):
     # other configs
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://envs.sh/RQS.jpg")
-    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '841851780').split()]
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
 
     # channels logs
     FORCE_SUB   = os.environ.get("FORCE_SUB", "NeonFiles") 
