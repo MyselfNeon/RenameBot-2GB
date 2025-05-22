8a033f1f863abf6ebd422f586271f9f08a5a056b
NeonAnurag <justasneon@gmail.com>
2025-05-22 08:32:31 +0530

    Update thumbnail.py

--- plugins/thumbnail.py
+++ plugins/thumbnail.py
@@ -8,18 +8,18 @@ async def viewthumb(client, message):
     if thumb:
        await client.send_photo(chat_id=message.chat.id, photo=thumb)
     else:
-        await message.reply_text("<b><i>You Don't Have Any Thumbnail ❌</i></b>") 
+        await message.reply_text("<b><i>Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Aɴʏ Tʜᴜᴍʙɴᴀɪʟ ❌</i></b>") 
 		
 @Client.on_message(filters.private & filters.command(['del_thumb', 'delthumb']))
 async def removethumb(client, message):
     await jishubotz.set_thumbnail(message.from_user.id, file_id=None)
-    await message.reply_text("<b><i>Thumbnail Deleted Successfully 🗑️</i></b>")
+    await message.reply_text("<b><i>Tʜᴜᴍʙɴᴀɪʟ Dᴇʟᴇᴛᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ 🗑️</i></b>")
 	
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client, message):
-    mkn = await message.reply_text("<b><i>Please Wait ...</i></b>")
+    mkn = await message.reply_text("<b><i>Pʟᴇᴀsᴇ Wᴀɪᴛ ...</i></b>")
     await jishubotz.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
-    await mkn.edit("<i><b>Thumbnail Saved Successfully ✅️</b></i>")
+    await mkn.edit("<i><b>Tʜᴜᴍʙɴᴀɪʟ Sᴀᴠᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ ✅️</b></i>")
 
 
 
