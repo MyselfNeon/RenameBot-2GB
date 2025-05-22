1913ff6cd6de403bab0280afe38eedfd235e6231
NeonAnurag <justasneon@gmail.com>
2025-05-22 07:14:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config(object):
  
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "https://envs.sh/RQS.jpg")
+    START_PIC   = os.environ.get("START_PIC", "https://envs.sh/rxA.jpg")
     ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
 
     # channels logs
