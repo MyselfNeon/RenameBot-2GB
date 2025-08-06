fdd3cf1ee5257a36a4f096a6b3e862dfe4b062bc
NeonAn <justasneon@gmail.com>
2025-08-06 17:57:17 +0530

    Add files via upload

--- plugins/admin_panel.py
+++ plugins/admin_panel.py
@@ -6,52 +6,44 @@ from helper.database import jishubotz
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
 
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
- 
-
-
-
 
 @Client.on_message(filters.command(["stats", "status", "s"]) & filters.user(Config.ADMIN))
 async def get_stats(bot, message):
     total_users = await jishubotz.total_users_count()
     uptime = time.strftime("%Hh%Mm%Ss", time.gmtime(time.time() - bot.uptime))    
     start_t = time.time()
-    st = await message.reply('**Processing The Details.....**')    
+    st = await message.reply('**__Pʀᴏᴄᴇssɪɴɢ Tʜᴇ Dᴇᴛᴀɪʟs ...__**')    
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
-    await st.edit(text=f"**--Bot Status--** \n\n**⌚ Bot Uptime:** `{uptime}` \n**🐌 Current Ping:** `{time_taken_s:.3f} ms` \n**👭 Total Users:** `{total_users}`")
-
+    await st.edit(text=f"**--Bᴏᴛ Sᴛᴀᴛᴜs--** \n\n**⌚ Bᴏᴛ Uᴘᴛɪᴍᴇ:** `{uptime}` \n**🐌 Cᴜʀʀᴇɴᴛ Pɪɴɢ:** `{time_taken_s:.3f} ms` \n**👭 Tᴏᴛᴀʟ Usᴇʀs:** `{total_users}`")
 
 
 @Client.on_message(filters.command(["restart", "r"]) & filters.user(Config.ADMIN))
 async def restart_bot(bot, message):
-    msg = await bot.send_message(text="**__Processes Stopped__** ❌ \n**__Bot Is Restarting__**...", chat_id=message.chat.id)       
+    msg = await bot.send_message(text="**__Pʀᴏᴄᴇss Sᴛᴏᴘᴘᴇᴅ__** ❌ \n\n**__Bᴏᴛ Is Rᴇsᴛᴀʀᴛɪɴɢ__**...", chat_id=message.chat.id)       
     await asyncio.sleep(3)
-    await msg.edit("**__Bot Is Restarted__** ♻️ \n**__Now You Can Use Me__** ⚡")
+    await msg.edit("**__Bᴏᴛ Is Rᴇsᴛᴀʀᴛᴇᴅ__** ♻️ \n\n**__Nᴏᴡ Yᴏᴜ Cᴀɴ Usᴇ Mᴇ__** ⚡")
     os.execl(sys.executable, sys.executable, *sys.argv)
-
 
 
 @Client.on_message(filters.private & filters.command(["ping", "p"]))
 async def ping(_, message):
     start_t = time.time()
-    rm = await message.reply_text("**__Pinging__**....")
+    rm = await message.reply_text("**__Pɪɴɢɪɴɢ__**....")
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
-    await rm.edit(f"**Ping** 🔥!\n{time_taken_s:.3f} ms")
+    await rm.edit(f"**__Pɪɴɢ__** 🔥!\n{time_taken_s:.3f} ms")
     return time_taken_s
-
 
 
 @Client.on_message(filters.command(["broadcast", "b"]) & filters.user(Config.ADMIN) & filters.reply)
 async def broadcast_handler(bot: Client, m: Message):
-    await bot.send_message(Config.LOG_CHANNEL, f"{m.from_user.mention} or {m.from_user.id} **__Is Started The Broadcast__**......")
+    await bot.send_message(Config.LOG_CHANNEL, f"{m.from_user.mention} Oʀ {m.from_user.id} **__Sᴛᴀʀᴛᴇᴅ Tʜᴇ Bʀᴏᴀᴅᴄᴀsᴛ__**...")
     all_users = await jishubotz.get_all_users()
     broadcast_msg = m.reply_to_message
-    sts_msg = await m.reply_text("**__Broadcast Started__**..!") 
+    sts_msg = await m.reply_text("**__Bʀᴏᴀᴅᴄᴀsᴛ Sᴛᴀʀᴛᴇᴅ__**..!") 
     done = 0
     failed = 0
     success = 0
@@ -67,9 +59,9 @@ async def broadcast_handler(bot: Client, m: Message):
            await jishubotz.delete_user(user['_id'])
         done += 1
         if not done % 20:
-           await sts_msg.edit(f"**__Broadcast In Progress:__** \n\nTotal Users {total_users} \nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed: {failed}")
+           await sts_msg.edit(f"**__Bʀᴏᴀᴅᴄᴀsᴛ Iɴ Pʀᴏɢʀᴇss:__** \n\nTᴏᴛᴀʟ Usᴇʀs {total_users} \nCᴏᴍᴘʟᴇᴛᴇᴅ: {done} / {total_users}\nSᴜᴄᴄᴇss: {success}\nFᴀɪʟᴇᴅ: {failed}")
     completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
-    await sts_msg.edit(f"**__Broadcast Completed:__** \n\nCompleted In `{completed_in}`.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed: {failed}")
+    await sts_msg.edit(f"**__Bʀᴏᴀᴅᴄᴀsᴛ Cᴏᴍᴘʟᴇᴛᴇᴅ:__** \n\nCᴏᴍᴘʟᴇᴛᴇᴅ Iɴ `{completed_in}`.\n\nTᴏᴛᴀʟ Usᴇʀs {total_users}\nCᴏᴍᴘʟᴇᴛᴇᴅ: {done} / {total_users}\nSᴜᴄᴄᴇss: {success}\nFᴀɪʟᴇᴅ: {failed}")
            
 async def send_msg(user_id, message):
     try:
@@ -79,22 +71,16 @@ async def send_msg(user_id, message):
         await asyncio.sleep(e.value)
         return send_msg(user_id, message)
     except InputUserDeactivated:
-        logger.info(f"{user_id} : **Deactivated**")
+        logger.info(f"{user_id} : **__Dᴇᴀᴄᴛɪᴠᴀᴛᴇᴅ__**")
         return 400
     except UserIsBlocked:
-        logger.info(f"{user_id} : **Blocked The Bot**")
+        logger.info(f"{user_id} : **__Bʟᴏᴄᴋᴇᴅ Tʜᴇ Bᴏᴛ__**")
         return 400
     except PeerIdInvalid:
-        logger.info(f"{user_id} : **User ID Invalid**")
+        logger.info(f"{user_id} : **__Usᴇʀ ID Iɴᴠᴀʟɪᴅ__**")
         return 400
     except Exception as e:
         logger.error(f"{user_id} : {e}")
         return 500
  
-
-
-
-
-
-
 
